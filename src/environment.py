b2e7835e6af0bc75dc47809ac9822dac439ff6fc
mikhailripp <ripp.mv@phystech.ru>
2022-05-06 22:47:33 +0300

    Fixed mistakes

--- src/environment.py
+++ src/environment.py
@@ -1,20 +1,41 @@
 import pygame
-from src.dots import Dot
+import src.settings as settings
 
-WIDTH = 1080
-HEIGHT = 920
-LIGHTBLUE = (0, 255, 255)
+pygame.init()
+screen = pygame.display.set_mode((int(settings.WIDTH), int(settings.HEIGHT)))
+
+
+class Dot(pygame.sprite.Sprite):
+    def __init__(self, center_x, center_y, first_x, first_y, second_x, second_y, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((settings.CELL, settings.CELL))
+        self.surf = pygame.image.load(img)
+        self.surf.set_colorkey((255, 255, 255))
+        self.surf = pygame.transform.scale(self.surf, (settings.SIZE * 256, settings.SIZE * 32))
+        self.image.blit(self.surf, (0, 0),
+                        (first_x, first_y, second_x, second_y))
+        self.rect = self.image.get_rect(center=(center_x, center_y))
 
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, x, y, filename):
+    def __init__(self, center_x, center_y, first_x, first_y, second_x,
+                 second_y, img):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(filename)
-        self.rect = self.image.get_rect(center=(x, y))
+        self.image = pygame.Surface((settings.CELL, settings.CELL))
+        self.surf = pygame.image.load(img)
+        self.surf.set_colorkey((255, 255, 255))
+        self.surf = pygame.transform.scale(self.surf, (settings.SIZE * 256, settings.SIZE * 32))
+        self.image.blit(self.surf, (0, 0),
+                        (first_x, first_y, second_x, second_y))
+        self.rect = self.image.get_rect(center=(center_x, center_y))
 
     def check_point(self, point):
-        return self.rect.topleft[0] <= point[0] <= self.rect.bottomright[0] and self.rect.topleft[1] <= point[1] <= \
-               self.rect.bottomright[1]
+        return self.rect.topleft[0] <= point[0] <= self.rect.bottomright[0] and \
+               self.rect.topleft[1] <= point[1] <= self.rect.bottomright[1]
+
+
+file = open("map/map.txt", 'r')
+map_carta = list(file)
 
 
 class Environment(object):
@@ -24,123 +45,50 @@ class Environment(object):
         self.create_self()
 
     def create_self(self):
-        self.group_blocks.add(Block(4, 168, 'blocks/verticalline.png'))
-        self.group_blocks.add(Block(4, 752, 'blocks/verticalline.png'))
-        self.group_blocks.add(Block(148, 4, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(752, 4, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(420, 4, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(920, 4, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(1076, 168, 'blocks/verticalline.png'))
-        self.group_blocks.add(Block(1076, 752, 'blocks/verticalline.png'))
-        self.group_blocks.add(Block(148, 916, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(752, 916, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(420, 916, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(920, 916, 'blocks/horizontalline.png'))
-        self.group_blocks.add(Block(98, 98, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(98, 238, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(98, 822, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(982, 98, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(982, 822, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(98, 682, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(982, 238, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(982, 682, 'blocks/square100x100.png'))
-        self.group_blocks.add(Block(288, 98, 'blocks/rectangle100x200.png'))
-        self.group_blocks.add(Block(792, 98, 'blocks/rectangle100x200.png'))
-        self.group_blocks.add(Block(288, 822, 'blocks/rectangle100x200.png'))
-        self.group_blocks.add(Block(792, 822, 'blocks/rectangle100x200.png'))
-        self.group_blocks.add(Block(540, 74, 'blocks/rectanglecolored.png'))
-        self.group_blocks.add(Block(540, 846, 'blocks/rectanglecolored.png'))
-        self.group_blocks.add(Block(74, 384, 'blocks/rectangleleft.png'))
-        self.group_blocks.add(Block(74, 536, 'blocks/rectangleleft.png'))
-        self.group_blocks.add(Block(1006, 384, 'blocks/rectangleright.png'))
-        self.group_blocks.add(Block(1006, 536, 'blocks/rectangleright.png'))
-        self.group_blocks.add(Block(213, 314, 'blocks/rectanglerightdown.png'))
-        self.group_blocks.add(Block(213, 606, 'blocks/rectanglerightup.png'))
-        self.group_blocks.add(Block(349, 536, 'blocks/rectangleleftbig.png'))
-        self.group_blocks.add(Block(731, 536, 'blocks/rectanglerightbig.png'))
-        self.group_blocks.add(Block(867, 314, 'blocks/rectangleleftdown.png'))
-        self.group_blocks.add(Block(867, 606, 'blocks/rectangleleftup.png'))
-        self.group_blocks.add(Block(349, 384, 'blocks/rectangleleftbig.png'))
-        self.group_blocks.add(Block(731, 384, 'blocks/rectanglerightbig.png'))
-        self.group_blocks.add(Block(540, 238, 'blocks/rectangledown.png'))
-        self.group_blocks.add(Block(540, 616, 'blocks/halfrectangledown.png'))
-        self.group_blocks.add(Block(540, 304, 'blocks/halfrectangleup.png'))
-        self.group_blocks.add(Block(540, 460, 'blocks/center.png'))
-        self.group_blocks.add(Block(540, 682, 'blocks/rectangleup.png'))
-        for i in range(28, 388, WIDTH // 40):
-            if i != 28 + WIDTH // 40 * 5:
-                self.group_dots.add(Dot(10, 10, i, 28, LIGHTBLUE))
-        for i in range(672, 1052, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 28, LIGHTBLUE))
-        for i in range(28, 388, WIDTH // 40):
-            if i != 28 + WIDTH // 40 * 5:
-                self.group_dots.add(Dot(10, 10, i, 892, LIGHTBLUE))
-        for i in range(699, 1050, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 892, LIGHTBLUE))
-        for i in range(5, 1080, WIDTH // 40):
-            if i != 5 + WIDTH // 40 * 33 and i != 5 + WIDTH // 40 * 17 and i != 5 + WIDTH // 40 * 18 and i != 5 + WIDTH // 40 * 19 and i != 5 + WIDTH // 40 * 20 and i != 5 + WIDTH // 40 * 21 and i != 5 + WIDTH // 40 * 22 and i != 5 + WIDTH // 40 * 34:
-                self.group_dots.add(Dot(10, 10, i, 460, LIGHTBLUE))
-        for i in range(55, 1080, WIDTH // 40):
-            if i != 55 + WIDTH // 40 * 4 and i != 55 + WIDTH // 40 * 32 and i != 55 + WIDTH // 40 * 37:
-                self.group_dots.add(Dot(10, 10, i, 168, LIGHTBLUE))
-        for i in range(55, 1080, WIDTH // 40):
-            if i != 55 + WIDTH // 40 * 4 and i != 55 + WIDTH // 40 * 32 and i != 55 + WIDTH // 40 * 37:
-                self.group_dots.add(Dot(10, 10, i, 752, LIGHTBLUE))
-        for i in range(290, 490, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 310, LIGHTBLUE))
-        for i in range(290, 470, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 610, LIGHTBLUE))
-        for i in range(617, 820, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 310, LIGHTBLUE))
-        for i in range(617, 820, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 610, LIGHTBLUE))
-        for i in range(28, 148, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 310, LIGHTBLUE))
-        for i in range(55, 148, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 610, LIGHTBLUE))
-        for i in range(927, 1080, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 310, LIGHTBLUE))
-        for i in range(940, 1040, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, i, 610, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 510, 400, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 537, 400, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 564, 400, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 510, 520, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 537, 520, LIGHTBLUE))
-        self.group_dots.add(Dot(10, 10, 564, 520, LIGHTBLUE))
-        # #по вертикали
-        for i in range(28, 280, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 28, i, LIGHTBLUE))
-        for i in range(612, 870, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 28, i, LIGHTBLUE))
-        for i in range(28, HEIGHT - 20, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 168, i, LIGHTBLUE))
-        for i in range(28, 148, WIDTH // 40):
-            self.group_dots.add((Dot(10, 10, 408, i, LIGHTBLUE)))
-        for i in range(28, 148, WIDTH // 40):
-            self.group_dots.add((Dot(10, 10, 672, i, LIGHTBLUE)))
-        for i in range(780, 900, WIDTH // 40):
-            self.group_dots.add((Dot(10, 10, 408, i, LIGHTBLUE)))
-        for i in range(780, 900, WIDTH // 40):
-            self.group_dots.add((Dot(10, 10, 672, i, LIGHTBLUE)))
-        for i in range(55, HEIGHT - 40, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 912, i, LIGHTBLUE))
-        for i in range(60, 280, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 1052, i, LIGHTBLUE))
-        for i in range(612, HEIGHT - 20, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 1052, i, LIGHTBLUE))
-        for i in range(195, 310, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 258, i, LIGHTBLUE))
-        for i in range(610, 732, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 258, i, LIGHTBLUE))
-        for i in range(195, 310, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 822, i, LIGHTBLUE))
-        for i in range(610, 732, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 822, i, LIGHTBLUE))
-        for i in range(310, 632, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 480, i, LIGHTBLUE))
-        for i in range(310, 632, WIDTH // 40):
-            self.group_dots.add(Dot(10, 10, 600, i, LIGHTBLUE))
+        for i in range(settings.MAP_SKETCH_WIDTH):
+            for j in range(settings.MAP_SKETCH_HEIGHT):
+                if map_carta[i][j] == 'o':
+                    self.group_dots.add(
+                        Dot(i * settings.CELL, j * settings.CELL, settings.CELL, settings.CELL, settings.CELL,
+                            settings.CELL,
+                            'blocks/map16.png'))
+
+                elif map_carta[i][j] == '.':
+                    try:
+                        self.group_dots.add(
+                            Dot(i * settings.CELL, j * settings.CELL, 0, settings.CELL, settings.CELL, settings.CELL,
+                                'blocks/map16.png'))
+                    except:
+                        pass
+
+                elif map_carta[i][j] == '#':
+                    down = 0
+                    left = 0
+                    right = 0
+                    up = 0
+                    if j < settings.MAP_SKETCH_HEIGHT - 1:
+                        if '#' == map_carta[i][1 + j]:
+                            down = 1
+                    if i > 0:
+                        if '#' == map_carta[i - 1][j]:
+                            left = 1
+                    else:
+                        left = 1
+                    if i < settings.MAP_SKETCH_WIDTH - 1:
+                        if '#' == map_carta[1 + i][j]:
+                            right = 1
+                    else:
+                        right = 1
+                    if j > 0:
+                        if '#' == map_carta[i][j - 1]:
+                            up = 1
+                    try:
+
+                        self.group_blocks.add(Block(i * settings.CELL, j * settings.CELL, settings.CELL * (
+                                down + 2 * (left + 2 * (right + 2 * up))), 0, settings.CELL,
+                                                    settings.CELL, 'blocks/map16.png'))
+                    except:
+                        pass
 
 
 env = Environment()
--- src/game.py
+++ src/game.py
@@ -1,31 +1,25 @@
-from src.creatures import *
-from src.menu import *
+from src.creatures import Enemies, Player
+import src.settings as settings
 import time
-
-WIDTH = 1080
-HEIGHT = 920
-FPS = 30
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+import pygame
+from src.environment import env
 
 
 class GamePlayFirstLevel(object):
-    def __init__(self, screen, game_over):
+    def __init__(self, screen, game_over, clock):
         self.game_over = game_over
         self.win = False
+        self.clock = clock
         self.environment = env
         self.screen = screen
-        self.player = Player(30, 30, "images/player.png", 'images/red_heart.png', 0)
+        self.player = Player(settings.WIDTH // 2, settings.HEIGHT // 2 + settings.CELL // 2, "images/player.png",
+                             'images/red_heart.png', 0)
         self.enemies_group = pygame.sprite.Group()
         self.make_enemies()
 
     def make_enemies(self):
-        for i in range(8):
-            self.enemies_group.add(Enemies('images/slime.png', WIDTH // 2, HEIGHT // 2))
+        for i in range(6):
+            self.enemies_group.add(Enemies('images/slime.png', settings.WIDTH // 2, settings.HEIGHT // 2 - 40))
 
     def events(self, events):
         for event in events:
@@ -61,18 +55,19 @@ class GamePlayFirstLevel(object):
             player.difference_y = 0
 
     def draw_result(self, phrase):
-        screen.fill(BLACK)
+        self.screen.fill(settings.BLACK)
         text = pygame.font.SysFont('arial', 50)
-        field = text.render(phrase, True, GREEN)
-        self.screen.blit(field, (WIDTH / 2 - field.get_width() / 2, HEIGHT / 2 - field.get_height() / 2))
+        field = text.render(phrase, True, settings.GREEN)
+        self.screen.blit(field,
+                         (settings.WIDTH // 2 - field.get_width() / 2, settings.HEIGHT // 2 - field.get_height() / 2))
 
     def moment_frame(self, *args):
         self.player.update()
-        self.screen.fill(BLACK)
+        self.screen.fill(settings.BLACK)
         self.enemies_group.update(*args)
         self.player.moment_image.set_colorkey((0, 0, 0))
         self.screen.blit(self.player.moment_image, self.player.rect)
-        self.environment.group_dots.draw(screen)
+        self.environment.group_dots.draw(self.screen)
         self.enemies_group.draw(self.screen)
         self.environment.group_blocks.draw(self.screen)
 
@@ -80,32 +75,44 @@ class GamePlayFirstLevel(object):
         self.moment_frame(self.player)
         text = pygame.font.SysFont('arial',
                                    self.player.moment_image.get_height())
-        field = text.render("Score: " + str(self.player.score), True, WHITE)
-        self.screen.blit(field, (WIDTH / 2 - field.get_width() / 2, 0))
+        field = text.render("Score: " + str(self.player.score), True, settings.WHITE)
+        self.screen.blit(field, (settings.WIDTH // 2 - field.get_width() / 2, 0))
         if self.win:
             self.draw_result("You won, congratulations!")
         if self.player.dead and self.player.animation_dead.moment_index == 11:
             self.draw_result("Score: " + str(self.player.score))
         pygame.display.flip()
 
+    def run_loop(self):
+        while not self.game_over:
+            events = pygame.event.get()
+            self.clock.tick(settings.FPS)
+            self.logic(self.player)
+            self.player.events_with_player(pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, self.win, events)
+            self.events(events)
+            self.draw_moment_frame()
+
 
 class GamePlayMultiplayer(GamePlayFirstLevel):
-    def __init__(self, screen, game_over):
-        GamePlayFirstLevel.__init__(self, screen, game_over)
-        self.player = Player(30, 30, 'images/player.png', 'images/red_heart.png', 3)
-        self.second_player = Player(WIDTH - 30, HEIGHT - 30, 'images/player.png', 'images/purple_heart.png', 3)
+    def __init__(self, screen, game_over, clock):
+        GamePlayFirstLevel.__init__(self, screen, game_over, clock)
+        self.player = Player(int(settings.CELL * 2), int(settings.CELL * 1), 'images/player.png',
+                             'images/red_heart.png', 3)
+        self.second_player = Player(int(settings.WIDTH - settings.CELL * 2) + settings.SIZE * 4,
+                                    int(settings.HEIGHT - settings.CELL * 2), 'images/player.png',
+                                    'images/purple_heart.png', 3)
 
     def draw_moment_frame(self):
         GamePlayFirstLevel.moment_frame(self, self.player, self.second_player)
         text = pygame.font.SysFont('arial', self.player.moment_image.get_height() // 2)
-        field = text.render("First Score: " + str(self.player.score), True, WHITE)
-        self.screen.blit(field, (WIDTH / 2 - field.get_width() / 2, 10))
+        field = text.render("First Score: " + str(self.player.score), True, settings.WHITE)
+        self.screen.blit(field, (settings.WIDTH // 2 - field.get_width() / 2, 10))
         text = pygame.font.SysFont('arial', self.second_player.moment_image.get_height() // 2)
-        field = text.render("Second Score: " + str(self.second_player.score), True, WHITE)
-        self.screen.blit(field, (WIDTH / 2 - field.get_width() / 2, 40))
+        field = text.render("Second Score: " + str(self.second_player.score), True, settings.WHITE)
+        self.screen.blit(field, (settings.WIDTH // 2 - field.get_width() / 2, 40))
         self.second_player.update()
         self.player.draw_lives(self.screen, 90)
-        self.second_player.draw_lives(self.screen, 45)
+        self.second_player.draw_lives(self.screen, -90)
         self.second_player.moment_image.set_colorkey((0, 0, 0))
         self.screen.blit(self.second_player.moment_image, self.second_player.rect)
         if self.win:
@@ -120,3 +127,15 @@ class GamePlayMultiplayer(GamePlayFirstLevel):
         elif self.second_player.dead and self.second_player.animation_dead.moment_index == 11:
             self.draw_result("Second lost, congratulations!")
         pygame.display.flip()
+
+    def run_multiplayer_loop(self):
+        while not self.game_over:
+            events = pygame.event.get()
+            self.clock.tick(settings.FPS)
+            self.logic(self.player)
+            self.logic(self.second_player)
+            self.player.events_with_player(pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, self.win, events)
+            self.second_player.events_with_player(pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT, self.win,
+                                                  events)
+            self.events(events)
+            self.draw_moment_frame()
